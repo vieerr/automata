88325811692a31592de24eaeb0cfd3c08706372e
vieerr <olivier_paspuel@hotmail.com>
2025-03-17 23:15:22 -0500

    Playable character, predefined maze with lighting details added

---
+++ main.lua
@@ -0,0 +1,178 @@
+local meshVertices = {} -- for fan mesh
+local meshRadius = 1
+table.insert(meshVertices, {0, 0, 0, 0, 1, 1, 1, 0}) -- white dot int the middle
+for angle = 0, 360, 10 do
+    local x = 0 + meshRadius * math.cos(math.rad(angle))
+    local y = 0 + meshRadius * math.sin(math.rad(angle))
+    table.insert(meshVertices, {x, y, 0, 0, 0, 0, 0, 1}) -- black dots on perimeter
+end
+Mesh = love.graphics.newMesh(meshVertices, "fan")
+
+local function updateCurtainCanvas(x, y, radius)
+    love.graphics.setCanvas(CurtainCanvas)
+    -- black curtain
+    love.graphics.setBlendMode("alpha")
+    love.graphics.setColor(0, 0, 0, 1) -- black
+    love.graphics.rectangle('fill', 0, 0, CurtainCanvas:getWidth(), CurtainCanvas:getHeight())
+
+    -- circle hole
+    love.graphics.setBlendMode("replace")
+    love.graphics.setColor(0, 0, 0, 0.97)
+    love.graphics.draw(Mesh, x, y, 0, radius)
+    love.graphics.setCanvas()
+end
+
+function love.load()
+    camera = require 'libraries/camera'
+    print(automata("vier"))
+    cam = camera()
+    cam.scale = 2
+    animation = newAnimation(love.graphics.newImage("ghost-Sheet.png"), 32, 32, 1)
+
+    -- set background
+    love.graphics.setBackgroundColor(255, 255, 255)
+
+    -- create canvas
+    CurtainCanvas = love.graphics.newCanvas()
+
+    -- update canvas
+    updateCurtainCanvas(100, 100, 100)
+
+    player = {
+        grid_x = 256,
+        grid_y = 256,
+        act_x = 200,
+        act_y = 200,
+        speed = 10
+    }
+
+     map = {
+        {0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
+        {0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+        {1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0},
+        {1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0},
+        {1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1},
+        {1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1},
+        {1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1},
+        {0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1},
+        {1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0},
+        {1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1},
+        {1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0},
+        {1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    }
+    
+    print(#map);
+    print(#map[1]);
+end
+
+function love.update(dt)
+
+    player.act_x = player.act_x - ((player.act_x - player.grid_x) * player.speed * dt)
+    player.act_y = player.act_y - ((player.act_y - player.grid_y) * player.speed * dt)
+    if love.keyboard.isDown("escape") then
+        love.event.quit()
+    end
+
+    cam:lookAt(player.act_x, player.act_y)
+    local w = love.graphics.getWidth()
+    local h = love.graphics.getHeight()
+
+    animation.currentTime = animation.currentTime + dt
+    if animation.currentTime >= animation.duration then
+        animation.currentTime = animation.currentTime - animation.duration
+    end
+    updateCurtainCanvas(player.act_x, player.act_y, 100)
+
+end
+
+function love.draw()
+    cam:attach()
+    -- restoring color settings
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.setBlendMode("alpha")
+
+    for y = 1, #map do
+        for x = 1, #map[y] do
+            if map[y][x] == 1 then
+                love.graphics.setColor(0, 0, 0)
+                love.graphics.rectangle("fill", x * 32, y * 32, 32, 32)
+            end
+        end
+    end
+
+    -- and than drawing curtain
+    love.graphics.draw(CurtainCanvas)
+    love.graphics.setColor(1, 1, 1)
+    -- love.graphics.rectangle("fill", player.act_x, player.act_y, 32, 32)
+
+    local spriteNum = math.floor(animation.currentTime / animation.duration * #animation.quads) + 1
+    love.graphics.draw(animation.spriteSheet, animation.quads[spriteNum], player.act_x, player.act_y)
+
+    cam:detach()
+    -- love.window.setFullscreen(true)
+
+    love.graphics.print("Position: " .. player.grid_x .. ", " .. player.grid_y, 10, 10)
+    love.graphics.print("moouse position: " .. love.mouse.getX() .. ", " .. love.mouse.getY(), 10, 30)
+end
+
+function love.keypressed(key, isrepeat)
+    if key == "up" then
+        if testMap(0, -1) then
+            player.grid_y = player.grid_y - 32
+        end
+    elseif key == "down" then
+        if testMap(0, 1) then
+            player.grid_y = player.grid_y + 32
+        end
+    elseif key == "left" then
+        if testMap(-1, 0) then
+            player.grid_x = player.grid_x - 32
+        end
+    elseif key == "right" then
+        if testMap(1, 0) then
+            player.grid_x = player.grid_x + 32
+        end
+    end
+end
+
+function testMap(x, y)
+    if map[(player.grid_y / 32) + y][(player.grid_x / 32) + x] == 1 then
+        return false
+    end
+    return true
+end
+
+function newAnimation(image, width, height, duration)
+    local animation = {}
+    animation.spriteSheet = image;
+    animation.quads = {};
+
+    for y = 0, image:getHeight() - height, height do
+        for x = 0, image:getWidth() - width, width do
+            table.insert(animation.quads, love.graphics.newQuad(x, y, width, height, image:getDimensions()))
+        end
+    end
+
+    animation.duration = duration or 1
+    animation.currentTime = 0
+
+    return animation
+end
+
+function automata(str)
+    local newstr = ""
+    for i = 1, #str do
+        local c = str:sub(i, i)
+        if c == "1" then
+            newstr = newstr .. "11"
+        else
+            newstr = newstr .. "00"
+        end
+    end
+    return newstr
+end
